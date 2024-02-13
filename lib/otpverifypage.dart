8488fad17447623e8ae2c90cd7cb84c150a0d3a8
mrisaac579 <mosesisaac579@gmail.com>
2024-02-13 15:34:22 +0100

    pin otp in progress

--- lib/otpverifypage.dart
+++ lib/otpverifypage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 // import 'package:pin_code_fields/pin_code_fields.dart';
 
 class OtpPage extends StatelessWidget {
@@ -15,29 +16,70 @@ class OtpPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      backgroundColor: Colors.lightGreen,
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 12, bottom: 0),
-                child: Text(
-                  'OTP code Verification',
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                        letterSpacing: .5,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
+      backgroundColor: Colors.green[50],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 12,
+                bottom: 80,
+              ),
+              child: Text(
+                'OTP code \n Verification',
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                      letterSpacing: .5,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              Text('code as been sent to +91 90901*****'),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: const Text('code has been send to +91 90901*****'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  PinCodeTextField(
+                    appContext: context,
+                    length: 4,
+                    pinTheme: PinTheme(
+                      shape: PinCodeFieldShape.box,
+                      borderRadius: BorderRadius.circular(8),
+                      fieldWidth: 70,
+                      selectedFillColor: Colors.white,
+                      selectedColor: Colors.white,
+                      activeFillColor: Colors.white,
+                      inactiveColor: Colors.white,
+                      inactiveFillColor: Colors.white,
+                      activeColor: Colors.white,
+                      borderWidth: 1,
+                      selectedBorderWidth: 4,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
