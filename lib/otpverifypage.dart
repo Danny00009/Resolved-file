f923e79fb4b994ec5b2670fe7d614c304d98a0e0
liftedman <gbeminiyiakinfala@gmail.com>
2024-02-14 10:34:52 +0100

    updated Otp

--- lib/otpverifypage.dart
+++ lib/otpverifypage.dart
@@ -18,129 +18,170 @@ class OtpPage extends StatelessWidget {
           ),
         ),
       ),
-      backgroundColor: Colors.green[50],
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 12,
-                bottom: 80,
-              ),
-              child: Text(
-                'OTP code \n Verification',
-                style: GoogleFonts.poppins(
-                  textStyle: const TextStyle(
-                      letterSpacing: .5,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold),
+      backgroundColor: Colors.teal[50],
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 12,
+                  bottom: 80,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 20,
-                right: 20,
-              ),
-              child: Text(
-                'Code has been send to +91 90901*****',
-                style: TextStyle(
-                  fontSize: 16,
-                  // fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
-              ),
-              child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  PinCodeTextField(
-                    appContext: context,
-                    length: 4,
-                    keyboardType: TextInputType.number,
-                    enablePinAutofill: true,
-                    pinTheme: PinTheme(
-                      shape: PinCodeFieldShape.box,
-                      borderRadius: BorderRadius.circular(9),
-                      fieldWidth: 80,
-                      selectedFillColor: Colors.white,
-                      selectedColor: Colors.white,
-                      activeFillColor: Colors.white,
-                      inactiveColor: Colors.white,
-                      inactiveFillColor: Colors.white,
-                      activeColor: Colors.white,
-                      borderWidth: 1,
-                      selectedBorderWidth: 2,
-                      // fieldOuterPadding: EdgeInsets.all(7),
-                    ),
-                    onChanged: (value) {},
+                child: Text(
+                  'OTP code \n Verification',
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                        letterSpacing: .5,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
                   ),
-                ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                right: 20,
+              const SizedBox(
+                height: 4,
               ),
-              child: RichText(
-                  text: TextSpan(
-                      text: ('Resend code in'),
-                      style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                        ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Text(
+                  'Code has been send to +91 90901*****',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    // fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    PinCodeTextField(
+                      appContext: context,
+                      length: 4,
+                      keyboardType: TextInputType.number,
+                      enablePinAutofill: true,
+                      backgroundColor: Colors.white,
+                      textStyle: TextStyle(
+                        fontSize: 30,
                       ),
-                      children: [
-                    TextSpan(
-                      text: (' 45'),
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 145, 222, 147),
+                      pinTheme: PinTheme(
+                        shape: PinCodeFieldShape.box,
+                        borderRadius: BorderRadius.circular(12),
+                        fieldWidth: 80,
+                        selectedFillColor: Colors.white,
+                        selectedColor: Colors.white,
+                        activeFillColor: Colors.white,
+                        inactiveColor: Colors.white,
+                        inactiveFillColor: Colors.white,
+                        activeColor: Colors.white,
+                        borderWidth: 1,
+                        selectedBorderWidth: 2,
+                        // fieldOuterPadding: EdgeInsets.all(7),
                       ),
+                      onChanged: (value) {},
                     ),
-                    TextSpan(
-                      text: (' s'),
-                    ),
-                  ])),
-            ),
-            Padding(
-                  padding: const EdgeInsets.only(
-                      bottom: 25, top: 80, left: 25, right: 25),
-                  child: InkWell(
-                    onTap: () {},
-                    child: Container(
-                      width: 400,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 53, 219, 142),
-                          borderRadius: BorderRadius.circular(8)),
-                      child: const Center(
-                        child: Text(
-                          'Verify',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 10,
+                ),
+                child: RichText(
+                    text: TextSpan(
+                        text: ('Resend code in'),
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
+                        children: [
+                      TextSpan(
+                        text: (' 45'),
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 1, 137, 124),
+                        ),
+                      ),
+                      TextSpan(
+                        text: (' s'),
+                      ),
+                    ])),
+              ),
+              SizedBox(
+                height: 170,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Container(
+                  width: double.maxFinite,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.teal.shade100,
+                          spreadRadius: 5,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                      color: const Color.fromARGB(255, 59, 168, 157),
+                      borderRadius: BorderRadius.circular(
+                        8,
+                      )),
+                  child: Center(
+                    child: Text(
+                      'Verify',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
                       ),
                     ),
                   ),
                 ),
-          ],
+              )
+              // InkWell(
+              //   onTap: () {},
+              //   child: Container(
+              //     width: 400,
+              //     height: 50,
+              //     decoration: BoxDecoration(
+              //         color: Color.fromARGB(255, 53, 219, 142),
+              //         borderRadius: BorderRadius.circular(8)),
+              //     child: const Center(
+              //       child: Text(
+              //         'Verify',
+              //         style: TextStyle(
+              //           fontSize: 20,
+              //           color: Colors.white,
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
